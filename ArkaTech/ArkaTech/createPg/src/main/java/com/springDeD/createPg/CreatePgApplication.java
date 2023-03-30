dd9430b0394b1b50b1fa8d80f4a607678cfaa3d2
Gabriele131099 <gabrisanna131099@gmail.com>
2023-03-30 20:59:43 +0200

    revisione front details pg

--- ArkaTech/ArkaTech/createPg/src/main/java/com/springDeD/createPg/CreatePgApplication.java
+++ ArkaTech/ArkaTech/createPg/src/main/java/com/springDeD/createPg/CreatePgApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CreatePgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreatePgApplication.class, args);
+		SpringApplication.run(CreatePgApplication.class, args); 
 	}
 
 }
